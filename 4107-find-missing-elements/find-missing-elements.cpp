9ef26683e280d34eeea256de867c164e1f36e6a0
Ayan pal <127892351+ayanpal01@users.noreply.github.com>
2026-08-04 11:27:52 +0530

    Time: 4 ms (35.49%) | Memory: 33.8 MB (8.08%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,17 +1,22 @@
 class Solution {
 private:
     vector<int>res;
+    int MIN = INT_MAX;
+    int MAX = INT_MIN;
+    int n;
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for(int i=0;i<n-1;i++){
-            int next = nums[i]+1;
-            while(next!=nums[i+1]){
-                res.push_back(next);
-                next++;
-            }
+        set<int>st(nums.begin(),nums.end());
+        n = nums.size();
+        for(int i=0;i<n;i++){
+            MIN=min(MIN,nums[i]);
+            MAX=max(MAX,nums[i]);
         }
+
+        for(int i=MIN+1;i<MAX;i++){
+            if(st.count(i)==0) res.push_back(i);
+        }
+
         return res;
     }
 };
