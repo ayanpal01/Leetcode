bcc99161a6c7a32e1aaf77f3e0bb01faff36b37d
Ayan pal <127892351+ayanpal01@users.noreply.github.com>
2026-08-18 21:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (51.50%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        int maxele = *max_element(nums.begin(), nums.end());
         int n = nums.size();
-        unordered_map<int, int> mp;
+        if(n==k) return maxele;
 
-        for (int i=0; i<=n-k; i++) {
-            unordered_set<int> st;
-            int j=i;
-            while(j<i+k){
-                st.insert(nums[j]);
-                j++;
-            }
-            for (int x : st) mp[x]++;
-        }
+        vector<int>freq(maxele+1,0);
+
+        for(int num:nums) freq[num]++;
 
         int ans = -1;
-        for (auto& [num, count] : mp) {
-            if (count == 1) ans = max(ans, num);
+        if(k==1){
+            for(int x:nums){
+                if(freq[x]==1) ans = max(ans,x);
+            }
+            return ans;
         }
+
+        if(freq[nums[0]]==1) ans = max(ans,nums[0]);
+        if(freq[nums[n-1]]==1) ans = max(ans,nums[n-1]);
 
         return ans;
     }
